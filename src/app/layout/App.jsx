8077cb8e533cccbd8834883efc43b4b3de675b5d
Tandara Elena <tandaraelena@yahoo.com>
2019-06-03 17:13:08 +0100

    working on components hierarchy

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import { Button } from "semantic-ui-react";
+import EventDashboard from "../../features/event/eventDashboard/EventDashboard";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Events</h1>
+        <EventDashboard />
       </div>
     );
   }
