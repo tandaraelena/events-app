e07f095f5059a661e155f56f4056c048ec043a5c
Tandara Elena <tandaraelena@yahoo.com>
2019-06-03 17:47:21 +0100

    styled the navbar and still working on the project structure

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
-import { Button } from "semantic-ui-react";
-import EventDashboard from "../../features/event/eventDashboard/EventDashboard";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar/NavBar";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Events</h1>
-        <EventDashboard />
+      <div>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
       </div>
     );
   }
