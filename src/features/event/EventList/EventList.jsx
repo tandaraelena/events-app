8de139860960292267f24465d3b7f302e0514ee5
Tandara Elena <tandaraelena@yahoo.com>
2019-06-04 11:55:31 +0100

    destructuring static data and populating event list items with it

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import EventListItem from "./EventListItem";
+
 class EventList extends Component {
   render() {
+    const { events } = this.props;
     return (
       <div>
         <h1>Event List</h1>
-        <EventListItem />
-        <EventListItem />
-        <EventListItem />
+        {events.map(event => (
+          <EventListItem key={event.id} event={event} />
+        ))}
       </div>
     );
   }
