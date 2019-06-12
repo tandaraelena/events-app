c0d2f74aa06f2b2c3bdc3032e0e09c69a7652e34
Tandara Elena <tandaraelena@yahoo.com>
2019-06-12 20:33:19 +0100

    extract the input information of the form and add more avatar pictures

--- src/features/event/eventDashboard/EventDashboard.jsx
+++ src/features/event/eventDashboard/EventDashboard.jsx
@@ -14,17 +14,17 @@ const eventsDashboard = [
     city: "London, UK",
     venue: "Tower of London, St Katharine's & Wapping, London",
     hostedBy: "Bob",
-    hostPhotoURL: "https://randomuser.me/api/portraits/men/20.jpg",
+    hostPhotoURL: "/assets/images/5.png",
     attendees: [
       {
         id: "a",
         name: "Bob",
-        photoURL: "https://randomuser.me/api/portraits/men/20.jpg"
+        photoURL: "/assets/images/images.jpg"
       },
       {
         id: "b",
         name: "Tom",
-        photoURL: "https://randomuser.me/api/portraits/men/22.jpg"
+        photoURL: "/assets/images/test1.png"
       }
     ]
   },
@@ -38,17 +38,17 @@ const eventsDashboard = [
     city: "London, UK",
     venue: "Punch & Judy, Henrietta Street, London, UK",
     hostedBy: "Tom",
-    hostPhotoURL: "https://randomuser.me/api/portraits/men/22.jpg",
+    hostPhotoURL: "/assets/images/5.png",
     attendees: [
       {
         id: "b",
         name: "Tom",
-        photoURL: "https://randomuser.me/api/portraits/men/22.jpg"
+        photoURL: "/assets/images/images.jpg"
       },
       {
         id: "a",
         name: "Bob",
-        photoURL: "https://randomuser.me/api/portraits/men/20.jpg"
+        photoURL: "/assets/images/test1.png"
       }
     ]
   }
