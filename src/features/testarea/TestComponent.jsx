89de9fc99a62385b0a2163c55bcee48bca1b8b03
tandaraelena <tandaraelena@yahoo.com>
2019-07-06 17:58:11 +0100

    create an action

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
+import { incrementCounter, decrementCounter } from "./testActions";
+import { Button } from "semantic-ui-react";
 
 const mapStateToProps = ( state ) => ({
   data: state.data
 })
 
+const actions = {
+  incrementCounter,
+  decrementCounter
+}
+
 class TestComponent extends Component {
   render() {
+    const { data, incrementCounter, decrementCounter } = this.props;
     return (
       <div>
         <h1>Test Component</h1>
         <h3>The answer is: {this.props.data}</h3>
+        <Button onClick={incrementCounter} positive content="Increment"/>
+        <Button onClick={decrementCounter} negative content="Decrement"/>
       </div>
     )
   }
 }
-export default connect(mapStateToProps)(TestComponent)
+export default connect(mapStateToProps, actions)(TestComponent)
