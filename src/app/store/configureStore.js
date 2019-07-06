0bb72915ccf98a890b936a5b0ff0f0b8bae819e8
tandaraelena <tandaraelena@yahoo.com>
2019-07-06 18:04:12 +0100

    create a rootReducer

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,9 +1,9 @@
 import { createStore } from "redux";
-import testReducer from "../../features/testarea/testReducer";
+import rootReducer from "../reducers/rootReducer";
 
 export const configureStore = () => {
-               // I created the store which takes first param as the reducer and second store enhancer
-  const store = createStore(testReducer)
+// I created the store which takes first param as the reducer and second store enhancer
+  const store = createStore(rootReducer)
 
   return store;
 }
