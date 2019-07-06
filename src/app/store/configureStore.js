1ff0e0c0b2799b45a2f10963dce9a8667cb46694
tandaraelena <tandaraelena@yahoo.com>
2019-07-06 18:22:42 +0100

    clean-up the code

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,9 +1,10 @@
 import { createStore } from "redux";
+import { devToolsEnhancer } from "redux-devtools-extension";
 import rootReducer from "../reducers/rootReducer";
 
 export const configureStore = () => {
 // I created the store which takes first param as the reducer and second store enhancer
-  const store = createStore(rootReducer)
+  const store = createStore(rootReducer, devToolsEnhancer())
 
   return store;
 }
