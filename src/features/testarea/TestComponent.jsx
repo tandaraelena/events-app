373f6c9444ff0f6eaee6eea1d4009ccf3386834d
tandaraelena <tandaraelena@yahoo.com>
2019-07-06 17:37:44 +0100

    connect the store to the app

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,12 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+
+const mapStateToProps = ( state ) => ({
+  data: state.data
+})
 
 class TestComponent extends Component {
   render() {
     return (
       <div>
         <h1>Test Component</h1>
+        <h3>The answer is: {this.props.data}</h3>
       </div>
     )
   }
 }
-export default TestComponent
+export default connect(mapStateToProps)(TestComponent)
