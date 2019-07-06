c1c41fdd25dd33f941cff5ee6209230b45fbc646
tandaraelena <tandaraelena@yahoo.com>
2019-07-06 14:28:14 +0100

    implement internal navigation to my SettingsDashboard component

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -18,17 +18,17 @@ class App extends Component {
         <Route 
           path="/(.+)" 
           render={() => (
-        <Fragment>
-        <NavBar />
-        <Container className="main">
-          <Route path="/events" component={EventDashboard}/>
-          <Route path="/events/:id" component={EventDetailedPage}/>
-          <Route path="/people" component={PeopleDashboard}/>
-          <Route path="/profile/:id" component={UserDetailedPage}/>
-          <Route path="/settings" component={SettingsDashboard}/>
-          <Route path="/createEvent" component={EventForm}/>
-        </Container>
-        </Fragment>
+            <Fragment>
+            <NavBar />
+            <Container className="main">
+              <Route path="/events" component={EventDashboard}/>
+              <Route path="/events/:id" component={EventDetailedPage}/>
+              <Route path="/people" component={PeopleDashboard}/>
+              <Route path="/profile/:id" component={UserDetailedPage}/>
+              <Route path="/settings" component={SettingsDashboard}/>
+              <Route path="/createEvent" component={EventForm}/>
+            </Container>
+            </Fragment>
       )} />
       </Fragment>
     );
