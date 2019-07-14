d7e84ae13302d678a7ea421c6d99a7909e0cf548
tandaraelena <tandaraelena@yahoo.com>
2019-07-14 14:52:12 +0100

    add the side bar into the event detailed page

--- src/features/event/EventDetailed/EventDetailedSideBar.jsx
+++ src/features/event/EventDetailed/EventDetailedSideBar.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Segment, Item, Label } from 'semantic-ui-react'
 
 const EventDetailedSideBar = () => {
   return (
-    <div>
-      <h1>SideBar</h1>
-    </div>
+        <Fragment>
+          <Segment
+            textAlign='center'
+            style={{ border: 'none' }}
+            attached='top'
+            secondary
+            inverted
+            color='teal'
+          >
+            2 People Going
+          </Segment>
+          <Segment attached>
+            <Item.Group divided>
+              <Item style={{ position: 'relative' }}>
+                <Label
+                  style={{ position: 'absolute' }}
+                  color='orange'
+                  ribbon='right'
+                >
+                  Host
+                </Label>
+                <Item.Image size='tiny' src='/assets/images/5.png' />
+                <Item.Content verticalAlign='middle'>
+                  <Item.Header as='h3'>Attendee Name</Item.Header>
+                </Item.Content>
+              </Item>
+            </Item.Group>
+          </Segment>
+        </Fragment>
   )
 }
 
