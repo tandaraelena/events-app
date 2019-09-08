a9fff3d1f09badef639635979358265195e8e56a
tandaraelena <tandaraelena@yahoo.com>
2019-09-08 08:52:04 +0100

    events-app challenge

--- src/features/testarea/SimpleMap.jsx
+++ src/features/testarea/SimpleMap.jsx
@@ -4,26 +4,23 @@ import { Icon } from 'semantic-ui-react'
 
 const AnyReactComponent = () => <Icon name='marker' size='big' color='red'/>
 class SimpleMap extends Component {
-  static defaultProps = {
-    center: {
-      lat: 59.95,
-      lng: 30.33
-    },
-    zoom: 6
-  };
+  // static defaultProps = {
+  //   center: {},
+  // };
 
   render() {
+    const { latlng } = this.props;
     return (
       // Important! Always set the container height explicitly
       <div style={{ height: '300px', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyBAXEJ4oAwmQcGKx9o43RwTrw_-tFZQOOw'}}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
+          defaultCenter={latlng}
+          defaultZoom={6}
         >
           <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
+            lat={latlng.lat}
+            lng={latlng.lng}
           />
         </GoogleMapReact>
       </div>
--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -4,6 +4,10 @@ import { incrementCounter, decrementCounter } from "./testActions";
 import { Button } from "semantic-ui-react";
 import TestPlaceInput from './TestPlaceInput'
 import SimpleMap from './SimpleMap';
+import {
+  geocodeByAddress,
+  getLatLng,
+} from 'react-places-autocomplete';
 
 const mapStateToProps = ( state ) => ({
   data: state.test.data
@@ -15,6 +19,24 @@ const actions = {
 }
 
 class TestComponent extends Component {
+  state = {
+    latlng: {
+      lat: 59.95,
+      lng: 30.33
+    }
+  }
+
+  handleSelect = address => {
+    geocodeByAddress(address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => {
+        this.setState({
+          latlng: latLng
+        })
+      })
+      .catch(error => console.error('Error', error));
+  };
+
   render() {
     const { incrementCounter, decrementCounter } = this.props;
     return (
@@ -24,8 +46,8 @@ class TestComponent extends Component {
         <Button onClick={incrementCounter} positive content="Increment"/>
         <Button onClick={decrementCounter} negative content="Decrement"/>
         <br/>
-        <TestPlaceInput />
-        <SimpleMap/>
+        <TestPlaceInput handleSelect={this.handleSelect} />
+        <SimpleMap key={this.state.latlng.lng} latlng={this.state.latlng} />
       </div>
     )
   }
