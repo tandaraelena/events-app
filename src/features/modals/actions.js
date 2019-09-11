8c196f407de3e1a44c632e3be6edd13a33b0656a
tandaraelena <tandaraelena@yahoo.com>
2019-09-11 20:42:47 +0100

    create a test modalReducer

---
+++ src/features/modals/actions.js
@@ -0,0 +1,11 @@
+import { MODAL_OPEN, MODAL_CLOSE } from "./modalConstants";
+
+export const openModal = (modalType, modalProps) => ({
+  type: MODAL_OPEN,
+  payload: {
+    modalType,
+    modalProps
+  }
+})
+
+export const closeModal = () => ({type: MODAL_CLOSE})
