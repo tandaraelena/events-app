950d4a6e080c1f129dd99c823ed6ea3709b89496
tandaraelena <tandaraelena@yahoo.com>
2019-09-29 11:07:58 +0100

    fetching events from the mock API

--- src/features/event/eventActions.js
+++ src/features/event/eventActions.js
@@ -1,4 +1,6 @@
-import { CREATE_EVENT, UPDATE_EVENT, DELETE_EVENT } from "./eventConstant";
+import { CREATE_EVENT, UPDATE_EVENT, DELETE_EVENT, FETCH_EVENTS } from "./eventConstant";
+import { asyncActionStarts, asyncActionFinish, asyncActionError } from '../async/async.actions'
+import { fetchSampleData } from "../../app/data/mockApi";
 
 // asta e actions creator care va returna event 
 export const createEvent = (event) => {
@@ -27,3 +29,17 @@ export const deleteEvent = (eventId) => {
     }
   }
 }
+export const loadEvents = () => {
+  return async dispatch => {
+    try {
+      dispatch(asyncActionStarts())
+      const events = await fetchSampleData()
+      dispatch({type: FETCH_EVENTS, payload: { events }})
+      dispatch(asyncActionFinish())
+    } catch (error) {
+      console.log(error);
+      dispatch(asyncActionError())
+    }
+  }
+}
+
