82cf99414782516af5430d57c09d3036a229954d
tandaraelena <tandaraelena@yahoo.com>
2019-09-29 13:10:23 +0100

    add a react-redux-toastr message when the event has been successfuly created or updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import 'react-redux-toastr/lib/css/react-redux-toastr.min.css'
 import "semantic-ui-css/semantic.min.css";
 import "./index.css";
 import App from "./app/layout/App.jsx";
@@ -9,6 +10,7 @@ import { Provider } from "react-redux";
 import { configureStore } from "./app/store/configureStore";
 import ScrollToTop from './app/common/util/ScrollToTop'
 import { loadEvents } from "./features/event/eventActions";
+import ReduxToastr from 'react-redux-toastr'
 
 const store = configureStore();
 store.dispatch(loadEvents())
@@ -20,6 +22,11 @@ let render = () => {
     <Provider store={store}>
       <BrowserRouter>
         <ScrollToTop>
+          <ReduxToastr
+            position='bottom-right'
+            transitionIn='fadeIn'
+            transitionOut='fadeOut'
+          />
           <App />
         </ScrollToTop>
       </BrowserRouter>
