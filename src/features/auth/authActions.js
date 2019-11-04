5f95c03c9a51e2fb14ffb5826585818e11b3470e
tandaraelena <tandaraelena@yahoo.com>
2019-11-04 21:11:05 +0000

    create a register method

--- src/features/auth/authActions.js
+++ src/features/auth/authActions.js
@@ -18,6 +18,29 @@ export const loginUser = (creds) => {
   }  
 }
 
+export const registerUser = user =>
+  async (dispatch, getState, { getFirebase, getFirestore }) => {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+    try {
+      let createdUser = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(user.email, user.password);
+      console.log(createdUser);
+      await createdUser.user.updateProfile({
+        displayName: user.displayName
+      })
+      let newUser = {
+        displayName: user.displayName,
+        createdAt: firestore.FieldValue.serverTimestamp()
+      };
+      await firestore.set(`users/${createdUser.user.uid}`, {...newUser})
+      dispatch(closeModal());
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
 export const signoutUser = () => {
   return {
     type: SIGN_OUT_USER
